20a71cca67b8eabd8716728b89e5c109e33d12cf
Jeff Carr <jcarr@wit.com>
2019-05-15 10:28:03 -0700

    this was what was causing it to crash. You can not Quit on other windows
    Signed-off-by: Jeff Carr <jcarr@wit.com>

--- demo.go
+++ demo.go
@@ -12,7 +12,7 @@ func SetupDemoUI() {
 	log.Println("setupDemoUI() START")
 	demowin = ui.NewWindow("Demo GUI Widgets", 500, 300, false)
 	demowin.OnClosing(func(*ui.Window) bool {
-		ui.Quit()
+		// ui.Quit()
 		return true
 	})
 	ui.OnShouldQuit(func() bool {
