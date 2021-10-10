38ad324ec0e715425be8fc58d73aec420e5acd95
Jeff Carr <jcarr@wit.com>
2021-10-09 19:25:52 -0500

    NODE: climbing up the rabbit hole

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func ExampleWindow() {
 	window := box.Window
 	log.Println("box =", box)
 	log.Println("window =", window)
-	// box.AddDebugTab("jcarr Debug")
+	node.AddDebugTab("jcarr Debug")
 
 	window.UiWindow.Show()
 }
@@ -50,7 +50,7 @@ func DebugWindow() {
 	window := box.Window
 	log.Println("box =", box)
 	log.Println("window =", window)
-	// box.AddDebugTab("jcarr Debug")
+	node.AddDebugTab("jcarr Debug")
 
 	window.UiWindow.Show()
 }
