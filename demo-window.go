8236be92dc72c2b443dc6ccfc2c2f6877af22afa
Jeff Carr <jcarr@wit.com>
2021-11-03 23:25:57 -0500

    EDITBOX: pull in a edit box example
    Signed-off-by: Jeff Carr <jcarr@wit.com>

--- demo-window.go
+++ demo-window.go
@@ -26,3 +26,48 @@ func (n *Node) AddGroup(title string) *Node {
 	newNode.uiBox = vbox
 	return newNode
 }
+
+func (n *Node) MakeBasicControlsPage(title string) *Node {
+	if (n == nil) {
+		return nil
+	}
+	origbox := n.uiBox
+	if (origbox == nil) {
+		return n
+	}
+
+	vbox := ui.NewVerticalBox()
+	vbox.SetPadded(true)
+
+	hbox := ui.NewHorizontalBox()
+	hbox.SetPadded(true)
+	vbox.Append(hbox, false)
+
+	hbox.Append(ui.NewButton("Button"), false)
+	hbox.Append(ui.NewCheckbox("Checkbox"), false)
+
+	vbox.Append(ui.NewLabel("This is a label. Right now, labels can only span one line."), false)
+
+	vbox.Append(ui.NewHorizontalSeparator(), false)
+
+	group := ui.NewGroup("Entries")
+	group.SetMargined(true)
+	vbox.Append(group, true)
+
+	group.SetChild(ui.NewNonWrappingMultilineEntry())
+
+	entryForm := ui.NewForm()
+	entryForm.SetPadded(true)
+	group.SetChild(entryForm)
+
+	entryForm.Append("Entry", ui.NewEntry(), false)
+	entryForm.Append("Password Entry", ui.NewPasswordEntry(), false)
+	entryForm.Append("Search Entry", ui.NewSearchEntry(), false)
+	entryForm.Append("Multiline Entry", ui.NewMultilineEntry(), true)
+	entryForm.Append("Multiline Entry No Wrap", ui.NewNonWrappingMultilineEntry(), true)
+
+	origbox.Append(vbox, false)
+	newNode := n.AddNode(title)
+	newNode.uiBox = vbox
+	return newNode
+}
