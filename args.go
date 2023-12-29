ced398383ee30b7fab4ff9e71cfa9d0ad96705b5
Jeff Carr <jcarr@wit.com>
2023-12-29 17:24:16 -0600

    using args.Register()
    Signed-off-by: Jeff Carr <jcarr@wit.com>

--- args.go
+++ args.go
@@ -4,19 +4,25 @@ import (
 	arg "github.com/alexflint/go-arg"
 )
 
-var guiArg GuiArgs
+var argGui ArgsGui
 
 // This struct can be used with the go-arg package
-type GuiArgs struct {
-	Gui string `arg:"--gui" help:"Use this gui toolkit [andlabs,gocui,nocui]"`
+type ArgsGui struct {
 	GuiDebug bool `arg:"--gui-debug" help:"open the GUI debugger"`
+	GuiPlugin string `arg:"--gui" help:"Use this gui toolkit [andlabs,gocui,nocui]"`
 	GuiVerbose bool `arg:"--gui-verbose" help:"enable all logging"`
 }
 
 func init() {
-	arg.Register(&guiArg)
+	arg.Register(&argGui)
 }
 
-func GetArg(a string) bool {
-	return guiArg.GuiDebug
+// returns the toolkit
+func ArgToolkit() string {
+	return argGui.GuiPlugin
+}
+
+// returns true if --gui-debug was passed from the command line
+func ArgDebug() bool {
+	return argGui.GuiDebug
 }
